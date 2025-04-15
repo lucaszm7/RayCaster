ca2bebd892148c36ca948bc529d76378e044c278
JoaoPedroRsilva <jprdsilva@inf.ufpel.edu.br>
2025-04-14 21:46:16 -0300

    Criaçao das structs Player e Boundary.

--- main.cpp
+++ main.cpp
@@ -13,6 +13,37 @@ public:
     int x = 0;
     int y = 0;
 
+	struct Player{
+		olc::vf2d posicao;
+		float raio;
+		// Construtor
+		Player(olc::PixelGameEngine* ptr, float a){
+			posicao = ptr->GetMousePos();
+			raio = a;
+		}
+		void drawPlayer(olc::PixelGameEngine* ptr){
+			ptr->FillCircle(posicao, raio, olc::BLUE);
+		}
+	};
+
+	struct Boundary{
+		float x1, y1;		
+		float x2, y2;
+		// Funçao construtora da struct Boundary
+		Boundary(float a, float b, float c, float d){
+			x1 = a;
+			y1 = b;
+			x2 = c;
+			y2 = d;
+		}
+		// Precisamos de um endereço do tipo olc::PixelGameEngine*
+		void draw(olc::PixelGameEngine* pt){
+			pt->DrawLine(x1, y1, x2, y2, olc::BLACK);
+		}
+	};
+
+	Boundary wall = Boundary(400, 150, 400, 350);
+
     RayCaster()
 	{
 		sAppName = "Ray Caster";
@@ -21,23 +52,23 @@ public:
 public:
 	bool OnUserCreate() override
 	{
-		
 		return true;
 	}
 
 	bool OnUserUpdate(float fElapsedTime) override
-	{
-		// Called once per frame, draws random coloured pixels
-		for (int x = 0; x < ScreenWidth(); x++)
-        {
-			for (int y = 0; y < ScreenHeight(); y++)
-            {
-				Draw(x, y, olc::Pixel(10000/(x + 1) % 255, 10000/(y + 1) % 255, 10000/(x + 1) % 255));
-            }
-        }
+	{	
+		// um é inicializado no OnUserUpdate para receber a posiçao do mouse em tempo real
+		Player um = Player(this, 5);
+		// Called once per frame
+		Clear(olc::WHITE);
+		// Ja que wall é objeto da struct Boundary e esta pertence a class RayCaster, entao o ponteiro this recebe a funçao DrawLine como herança 
+		wall.draw(this);
+		um.drawPlayer(this);
+
 		return true;
 	}
 };
+
 
 int main(int argc, char** argv)
 {
