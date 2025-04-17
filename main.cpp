5680925883c996241a1b7c97780cd37ff2e8a8e7
Lucas Morais <lucas.morais446@hotmail.com>
2025-04-16 23:01:41 -0300

    change to ray class

--- main.cpp
+++ main.cpp
@@ -9,52 +9,64 @@
 
 // g++ -fopenmp main.cpp -luser32 -lgdi32 -lopengl32 -lgdiplus -lShlwapi -ldwmapi -lstdc++fs -static -std=c++17 -O3 -o main
 
+struct Player
+{
+	olc::vf2d position;
+	float speed;
+	float raio;
+
+	Player(float x, float y, float speed, float raio)
+		: position(x, y), speed(speed), raio(raio)
+	{}
+
+	void drawPlayer(olc::PixelGameEngine* ptr)
+	{
+		ptr->FillCircle(position.x, position.y, raio, olc::RED);
+	}
+};
+
+
+struct Map
+{
+	int width;
+	int heigth;
+	int cellSize;
+	int stage[8][8] =  {0, 0, 0, 0, 0, 0, 0, 0,
+						0, 0, 0, 0, 0, 0, 0, 0,
+						0, 0, 0, 0, 0, 0, 0, 0,
+						0, 0, 0, 0, 0, 1, 1, 0,
+						0, 0, 0, 0, 0, 1, 1, 0,
+						0, 0, 0, 0, 0, 1, 1, 0,
+						0, 1, 0, 0, 0, 0, 0, 0,
+						0, 1, 0, 0, 0, 0, 0, 0,};
+
+	Map(int width, int heigth, int cellSize)
+		: width(width), heigth(heigth), cellSize(cellSize)
+	{}
+
+	const int* operator[](size_t row) const 
+	{
+        return stage[row];
+    }
+};
+
+struct Ray
+{
+	olc::vf2d origin;
+	olc::vf2d direction;
+
+	Ray(olc::vf2d origin, olc::vf2d direction)
+	: origin(origin), direction(direction) {}
+};
+
 class RayCaster : public olc::PixelGameEngine
 {
 public:
-
-    int x = 0;
-    int y = 0;
-
-	struct Player{
-		float posicaoX, posicaoY;
-		float speed;
-		float raio;
-		// Construtor
-		Player(float a, float b, float c, float d){
-			posicaoX = a;
-			posicaoY = b;
-			speed = c;
-			raio = d;
-		}
-
-		void drawPlayer(olc::PixelGameEngine* ptr){
-			ptr->FillCircle(posicaoX, posicaoY, raio, olc::RED);
-		}
-	};
-	Player um = Player(100, 100, 100, 5);
-
-	struct Map{
-		int largura, altura;
-		int tijolo;
-		int stage[8][8] =  {0, 0, 0, 0, 0, 0, 0, 0,
-							0, 0, 0, 0, 0, 0, 0, 0,
-							0, 0, 0, 0, 0, 0, 0, 0,
-							0, 0, 0, 0, 0, 1, 1, 0,
-							0, 0, 0, 0, 0, 1, 1, 0,
-							0, 0, 0, 0, 0, 1, 1, 0,
-							0, 1, 0, 0, 0, 0, 0, 0,
-							0, 1, 0, 0, 0, 0, 0, 0,};
-
-		Map(int a, int b, int c){
-			largura = a;
-			altura = b;
-			tijolo = c;
-		}
-	};
-	Map mapa = Map(8, 8, 64);
+	Player player;
+	Map map;
 
     RayCaster()
+	: player(100, 100, 100, 5), map(8, 8, 64)
 	{
 		sAppName = "Ray Caster";
 	}
@@ -67,55 +79,57 @@ public:
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{	
-		// Called once per frame
 		Clear(olc::BLACK);
 		olc::vi2d pos = GetMousePos();
-		// MAPA
 
-		for(int y = 0; y < mapa.altura; y++){
-			for(int x = 0; x < mapa.largura; x++){
-				if(mapa.stage[y][x] == 1){
-					FillRect(x*mapa.tijolo, y*mapa.tijolo, mapa.tijolo, mapa.tijolo, olc::WHITE);
+		for(int y = 0; y < map.heigth; y++)
+		{
+			for(int x = 0; x < map.width; x++)
+			{
+				if(map[y][x] == 1)
+				{
+					FillRect(x * map.cellSize, y * map.cellSize, map.cellSize, map.cellSize, olc::WHITE);
 				}
 			}
 		}
+
+		// olc::vf2d direction = pos - player.position;
+		// float hipotenusa = direction.mag();
+		// olc::vf2d directionNormalized = direction / hipotenusa;
+
 		// RAY
 		// Calcula a hipotenusa com a diferença dos eixos X e Y do jogador ate a posicao do mouse
-		float hipotenusa = sqrtf(powf((um.posicaoX - pos.x), 2) + powf((um.posicaoY - pos.y), 2));
+		float hipotenusa = sqrtf(powf((pos.x - player.position.x), 2) + powf((player.position.y - pos.y), 2));
 		// Calcula os seno e cosseno
-		float anguloYsin = (pos.y - um.posicaoY)/hipotenusa;
-		float anguloXcos = (pos.x - um.posicaoX)/hipotenusa; 
+		float anguloYsin = (pos.y - player.position.y)/hipotenusa;
+		float anguloXcos = (pos.x - player.position.x)/hipotenusa; 
+
 		// Acha o angulo entre jogador e mouse usando a funçao atan2()
 		float angulo = atan2f(anguloYsin, anguloXcos);
 		// Ideia inicializar o loop subtraindo 45 graus do angulo achado com atan2() e fazer o loop ate completar 90 graus
 		// Tava dando erro no loop pq a variavel angulo que checava o fim do loop era a mesma que incrementava, entao ficava infinito
-		for(float anguloLoop = angulo - (PI/4); anguloLoop < angulo + (PI/4); anguloLoop += 0.1){ 
+		for(float anguloLoop = angulo - (PI/4); anguloLoop < angulo + (PI/4); anguloLoop += 0.1)
+		{
 			// Calcula o seno e cosseno pra cada angulo do incremento do loop
-			float rayX = cos(anguloLoop);
-			float rayY = sin(anguloLoop);
-			// Calculo para o tamanho dos raios
-			float fimX = um.posicaoX + rayX * 500;
-			float fimY = um.posicaoY + rayY * 500;
-			DrawLine(um.posicaoX, um.posicaoY, fimX, fimY, olc::BLUE);
+			olc::vf2d dir(cos(anguloLoop), sin(anguloLoop));
+			Ray ray(player.position, dir);
+			olc::vf2d finalPosition = ray.origin + ray.direction * 500;
+			DrawLine(player.position.x, player.position.y, finalPosition.x, finalPosition.y, olc::BLUE);
 		}
-		DrawLine(um.posicaoX, um.posicaoY, pos.x, pos.y, olc::RED);
-		// MOVIMENTAÇAO
+		DrawLine(player.position.x, player.position.y, pos.x, pos.y, olc::RED);
 
-		if (GetKey(olc::W).bHeld) um.posicaoY -= um.speed * fElapsedTime;
-    	if (GetKey(olc::S).bHeld) um.posicaoY += um.speed * fElapsedTime;
-   		if (GetKey(olc::A).bHeld) um.posicaoX -= um.speed * fElapsedTime;
-   		if (GetKey(olc::D).bHeld) um.posicaoX += um.speed * fElapsedTime;
-		um.drawPlayer(this);
+		if (GetKey(olc::W).bHeld) player.position.y -= player.speed * fElapsedTime;
+    	if (GetKey(olc::S).bHeld) player.position.y += player.speed * fElapsedTime;
+   		if (GetKey(olc::A).bHeld) player.position.x -= player.speed * fElapsedTime;
+   		if (GetKey(olc::D).bHeld) player.position.x += player.speed * fElapsedTime;
+		player.drawPlayer(this);
 
 		return true;
 	}
 };
 
-
 int main(int argc, char** argv)
 {
-    std::cout << "Hello World\n";
-
     RayCaster application;
     if (application.Construct(512, 512, 1, 1))
     {
