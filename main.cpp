652aa9a94b41204ed82704d50986fbb5b2b13906
JoaoPedroRsilva <jprdsilva@inf.ufpel.edu.br>
2025-04-16 21:32:26 -0300

    Substitui a funçao Boundary pela Map. Mudanças na funçao Player adicionando movimento e raios.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #define OLC_PGE_APPLICATION
 
 #include <cmath>
+#include <math.h>
+#include <stdlib.h>
 #include <iostream>
 #include "olcPixelGameEngine.h"
+#define PI 3.141592653589793f
 
 // g++ -fopenmp main.cpp -luser32 -lgdi32 -lopengl32 -lgdiplus -lShlwapi -ldwmapi -lstdc++fs -static -std=c++17 -O3 -o main
 
@@ -14,35 +17,42 @@ public:
     int y = 0;
 
 	struct Player{
-		olc::vf2d posicao;
+		float posicaoX, posicaoY;
+		float speed;
 		float raio;
 		// Construtor
-		Player(olc::PixelGameEngine* ptr, float a){
-			posicao = ptr->GetMousePos();
-			raio = a;
+		Player(float a, float b, float c, float d){
+			posicaoX = a;
+			posicaoY = b;
+			speed = c;
+			raio = d;
 		}
+
 		void drawPlayer(olc::PixelGameEngine* ptr){
-			ptr->FillCircle(posicao, raio, olc::BLUE);
+			ptr->FillCircle(posicaoX, posicaoY, raio, olc::RED);
 		}
 	};
+	Player um = Player(100, 100, 100, 5);
 
-	struct Boundary{
-		float x1, y1;		
-		float x2, y2;
-		// Funçao construtora da struct Boundary
-		Boundary(float a, float b, float c, float d){
-			x1 = a;
-			y1 = b;
-			x2 = c;
-			y2 = d;
-		}
-		// Precisamos de um endereço do tipo olc::PixelGameEngine*
-		void draw(olc::PixelGameEngine* pt){
-			pt->DrawLine(x1, y1, x2, y2, olc::BLACK);
+	struct Map{
+		int largura, altura;
+		int tijolo;
+		int stage[8][8] =  {0, 0, 0, 0, 0, 0, 0, 0,
+							0, 0, 0, 0, 0, 0, 0, 0,
+							0, 0, 0, 0, 0, 0, 0, 0,
+							0, 0, 0, 0, 0, 1, 1, 0,
+							0, 0, 0, 0, 0, 1, 1, 0,
+							0, 0, 0, 0, 0, 1, 1, 0,
+							0, 1, 0, 0, 0, 0, 0, 0,
+							0, 1, 0, 0, 0, 0, 0, 0,};
+
+		Map(int a, int b, int c){
+			largura = a;
+			altura = b;
+			tijolo = c;
 		}
 	};
-
-	Boundary wall = Boundary(400, 150, 400, 350);
+	Map mapa = Map(8, 8, 64);
 
     RayCaster()
 	{
@@ -57,12 +67,44 @@ public:
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{	
-		// um é inicializado no OnUserUpdate para receber a posiçao do mouse em tempo real
-		Player um = Player(this, 5);
 		// Called once per frame
-		Clear(olc::WHITE);
-		// Ja que wall é objeto da struct Boundary e esta pertence a class RayCaster, entao o ponteiro this recebe a funçao DrawLine como herança 
-		wall.draw(this);
+		Clear(olc::BLACK);
+		olc::vi2d pos = GetMousePos();
+		// MAPA
+
+		for(int y = 0; y < mapa.altura; y++){
+			for(int x = 0; x < mapa.largura; x++){
+				if(mapa.stage[y][x] == 1){
+					FillRect(x*mapa.tijolo, y*mapa.tijolo, mapa.tijolo, mapa.tijolo, olc::WHITE);
+				}
+			}
+		}
+		// RAY
+		// Calcula a hipotenusa com a diferença dos eixos X e Y do jogador ate a posicao do mouse
+		float hipotenusa = sqrtf(powf((um.posicaoX - pos.x), 2) + powf((um.posicaoY - pos.y), 2));
+		// Calcula os seno e cosseno
+		float anguloYsin = (pos.y - um.posicaoY)/hipotenusa;
+		float anguloXcos = (pos.x - um.posicaoX)/hipotenusa; 
+		// Acha o angulo entre jogador e mouse usando a funçao atan2()
+		float angulo = atan2f(anguloYsin, anguloXcos);
+		// Ideia inicializar o loop subtraindo 45 graus do angulo achado com atan2() e fazer o loop ate completar 90 graus
+		// Tava dando erro no loop pq a variavel angulo que checava o fim do loop era a mesma que incrementava, entao ficava infinito
+		for(float anguloLoop = angulo - (PI/4); anguloLoop < angulo + (PI/4); anguloLoop += 0.1){ 
+			// Calcula o seno e cosseno pra cada angulo do incremento do loop
+			float rayX = cos(anguloLoop);
+			float rayY = sin(anguloLoop);
+			// Calculo para o tamanho dos raios
+			float fimX = um.posicaoX + rayX * 500;
+			float fimY = um.posicaoY + rayY * 500;
+			DrawLine(um.posicaoX, um.posicaoY, fimX, fimY, olc::BLUE);
+		}
+		DrawLine(um.posicaoX, um.posicaoY, pos.x, pos.y, olc::RED);
+		// MOVIMENTAÇAO
+
+		if (GetKey(olc::W).bHeld) um.posicaoY -= um.speed * fElapsedTime;
+    	if (GetKey(olc::S).bHeld) um.posicaoY += um.speed * fElapsedTime;
+   		if (GetKey(olc::A).bHeld) um.posicaoX -= um.speed * fElapsedTime;
+   		if (GetKey(olc::D).bHeld) um.posicaoX += um.speed * fElapsedTime;
 		um.drawPlayer(this);
 
 		return true;
@@ -75,7 +117,7 @@ int main(int argc, char** argv)
     std::cout << "Hello World\n";
 
     RayCaster application;
-    if (application.Construct(516, 516, 1, 1))
+    if (application.Construct(512, 512, 1, 1))
     {
         application.Start();
     }
