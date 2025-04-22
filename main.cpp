013a770da4dab61992f12666ac2943f8f3b87ec6
JoaoPedroRsilva <jprdsilva@inf.ufpel.edu.br>
2025-04-21 23:23:53 -0300

    wip

--- main.cpp
+++ main.cpp
@@ -132,27 +132,78 @@ public:
 		float angulo = atan2f(anguloYsin, anguloXcos);
 		// Ideia inicializar o loop subtraindo 45 graus do angulo achado com atan2() e fazer o loop ate completar 90 graus
 		// Tava dando erro no loop pq a variavel angulo que checava o fim do loop era a mesma que incrementava, entao ficava infinito
-		for(float anguloLoop = angulo - (PI/4); anguloLoop < angulo + (PI/4); anguloLoop += 0.01)
+		 for(float anguloLoop = angulo - (PI/4); anguloLoop < angulo + (PI/4); anguloLoop += 0.5)
 		{
 			// Calcula o seno e cosseno pra cada angulo do incremento do loop
-			olc::vf2d dir(cos(anguloLoop), sin(anguloLoop));
-			Ray ray(player.position, dir);
 			// olc::vf2d collisionPos = Collision(ray, map);
 			// float length = (collisionPos - ray.origin).mag();
-			float Ynearest, Xnearest;
-			Ynearest = map.cellSize - (map.cellSize - ((int)(player.position.y / map.cellSize) * map.cellSize));
-			Xnearest = Ynearest / anguloLoop;
-			olc::vi2d cell((Xnearest / map.cellSize), (Ynearest / map.cellSize));
-			float distance = 500;
-			if(map[cell.x][cell.y] == 1){
-				distance = sqrt(cell.x * cell.x + cell.y * cell.y);
+			// Horizontal
+			float nearestHorizontaly;
+			float nearestHorizontalx;
+			if(sin(anguloLoop) > 0){
+				nearestHorizontaly = map.cellSize - (player.position.y - floorf(player.position.y / map.cellSize) * map.cellSize);
+			}else {
+				nearestHorizontaly = player.position.y - floorf(player.position.y / map.cellSize) * map.cellSize;
 			}
-			float Ynext = map.cellSize;
-			float Xnext = Ynext / anguloLoop;
+			nearestHorizontalx = nearestHorizontaly / tan(anguloLoop);
+			olc::vf2d stepHorizontal (player.position.x, player.position.y);
+			int KeyH = 0;
+			if(map[(int)(nearestHorizontalx / map.cellSize)][(int)(nearestHorizontaly / map.cellSize)] == 1){
+				stepHorizontal.x += nearestHorizontalx;
+				stepHorizontal.y += nearestHorizontaly;
+				KeyH = 1;
+			}
+			olc::vf2d nextHorizontal;
+			nextHorizontal.y = map.cellSize;
+			nextHorizontal.x = nextHorizontal.y / tan(anguloLoop);
+			while(stepHorizontal.x < map.width && stepHorizontal.y < map.heigth && stepHorizontal.x > 0 && stepHorizontal.y > 0){
+				if(map[(int)(stepHorizontal.x / map.cellSize)][(int)(stepHorizontal.y / map.cellSize)] == 1){
+					break;
+				}
+				stepHorizontal.x += nextHorizontal.x;
+				stepHorizontal.y += nextHorizontal.y; 
+			}
+			// Vertical
+			olc::vf2d nearestVertical;
+			if(cos(anguloLoop) > 0){
+				nearestVertical.x = map.cellSize - (player.position.x - floorf(player.position.x / map.cellSize) * map.cellSize);
+			} else{
+				nearestVertical.x = player.position.x - floorf(player.position.x / map.cellSize) * map.cellSize;
+			}
+			
+			nearestVertical.y = nearestVertical.x * tan(anguloLoop);
+			olc::vf2d stepVertical(player.position.x, player.position.y);
+			int KeyV = 0;
+			if(map[(int)(nearestVertical.x / map.cellSize)][(int)(nearestVertical.y / map.cellSize)] == 1){
+				stepVertical.x += nearestVertical.x;
+				stepVertical.y += nearestVertical.y;
+				KeyV = 1;
+			}
+			olc::vf2d nextVertical;
+			nextVertical.x = map.cellSize;
+			nextVertical.y = nextVertical.x / tan(anguloLoop);
+			while(stepVertical.x < map.width && stepVertical.y < map.heigth && stepVertical.x > 0 && stepVertical.y > 0){
+				if(map[(int)(stepVertical.x / map.cellSize)][(int)(stepVertical.y / map.cellSize)] == 1){
+					break;
+				}
+				stepVertical.x += nextVertical.x;
+				stepVertical.y += nextVertical.y;
+			}
+			float distH = (stepHorizontal - player.position).mag();
+			float distV = (stepVertical - player.position).mag();
+			float distance = 0;
+			if(distH < distV){
+				distance = distH;
+			}else {
+				distance = distV;
+			}
+			olc::vf2d dir(cos(anguloLoop), sin(anguloLoop));
+			Ray ray(player.position, dir);
 
 			olc::vf2d finalPosition = ray.origin + ray.direction * distance;
 
 			DrawLine(player.position.x, player.position.y, finalPosition.x, finalPosition.y, olc::YELLOW);
+			anguloLoop = 3222;
 		}
 
 		if (GetKey(olc::W).bHeld) player.position.y -= player.speed * fElapsedTime;
